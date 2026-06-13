dc2669d96c33ee3afdc0dacb28c17c1c393bac4d
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-06-13 17:13:05 +0200

    chore(core): add TranslationResults class to define translation result structure

---
+++ src/Translator.Core/TranslationResults.cs
@@ -0,0 +1,8 @@
+namespace Translator.Core;
+
+public class TranslationResults
+{
+    public string Source { get; set; } = "";
+    public string Target { get; set; } = "";
+    public string Translation { get; set; } = "";
+}
